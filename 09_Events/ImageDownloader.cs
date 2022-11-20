6e2ca224cb20b0503d154d2180f1be8f77186c05
sias32 <sias320@gmail.com>
2022-11-20 14:18:16 +0300

    HomeWork_09

---
+++ 09_Events/ImageDownloader.cs
@@ -0,0 +1,45 @@
+﻿using System.Net;
+
+namespace ConsoleApp_NET6;
+
+internal class ImageDownloader
+{
+    public void Download(string url, string file)
+    {
+        // Для отображения имени сайта
+        Uri uri= new(url);
+        string fileName = file.Split('/')[file.Split('/').Length - 1];
+
+        Console.WriteLine($"Скачиваю с сайта: {uri.Host}\n");
+
+        Task taskStatus;
+        using (WebClient webClient = new())
+        {
+            taskStatus = webClient.DownloadFileTaskAsync(url, file);
+        }
+
+        while (true)
+        {
+            Console.WriteLine("Нажмите клавишу A для выхода или любую другую клавишу для проверки статуса скачивания");
+            ConsoleKeyInfo key = Console.ReadKey();
+
+            if (key.Key == ConsoleKey.A)
+            {
+                Console.WriteLine();
+                break;
+            }
+            else
+            {
+                if (taskStatus.IsCompleted == Task.CompletedTask.IsCompleted)
+                {
+                    Console.WriteLine("Скачалось");
+                }
+                else
+                {
+                    Console.WriteLine("Еще не скачалось");
+                }
+            }
+        }
+        Console.WriteLine($"Успешно сохранил как: {fileName}");
+    }
+}
---
+++ 09_Events/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace ConsoleApp_NET6;
+
+internal class Program
+{
+    public static event Action<int> ImageNews;
+
+    static void Main(string[] args)
+    {
+        // Добавление обработчика события
+        ImageNews += ImageHandler;
+
+        string url = "https://effigis.com/wp-content/uploads/2015/02/Iunctus_SPOT5_5m_8bit_RGB_DRA_torngat_mountains_national_park_8bits_1.jpg";
+        //Так файл скачивается в корень проекта
+        string file = "../../../bigimage.jpg";
+
+        ImageDownloader imageDownloader = new ImageDownloader();
+
+        ImageNews.Invoke(1);
+        imageDownloader.Download(url, file);
+        ImageNews.Invoke(0);
+    }
+
+    /// <summary>
+    /// Отлавливает события Event
+    /// </summary>
+    /// <param name="status">статус скачивания (1;0)</param>
+    private static void ImageHandler(int status)
+    {
+        if (status == 1)
+        {
+            Console.WriteLine("Скачивание файла началось");
+        }
+
+        if (status == 0)
+        {
+            Console.WriteLine("Скачивание файла закончилось");
+        }
+    }
+}
