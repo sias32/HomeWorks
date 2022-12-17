601c35953f034547bb60a32e6e05ce1f2406e2cb
sias32 <sias320@gmail.com>
2022-12-17 18:03:32 +0300

    new hw 11_Dictionary

---
+++ 11_Dictionary/OtusDictionary.cs
@@ -0,0 +1,134 @@
+﻿namespace Dictionary;
+
+/// <summary>
+/// Переменная, для хранения ключа и значеия
+/// </summary>
+internal class OtusDictionaryItem
+{
+    public int key;
+
+    public string value;
+
+    internal OtusDictionaryItem(int key, string value)
+    {
+        this.key = key;
+        this.value = value;
+    }
+}
+
+/// <summary>
+/// Словарь
+/// </summary>
+internal class OtusDictionary
+{
+    
+    private OtusDictionaryItem[] Book;
+
+    private int _size;
+
+    public int length => _size;
+
+    /// <summary>
+    /// Индексирование, позволяет заменить методы Add и Get
+    /// </summary>
+    /// <param name="i"></param>
+    /// <returns>Объект OtusDictionaryItem</returns>
+    public OtusDictionaryItem this[int i]
+    {
+        get
+        {
+            return Get(i);
+        }
+        set 
+        {
+            Add(value.key, value.value);
+        }
+    }
+
+    internal OtusDictionary()
+    {
+        Book = new OtusDictionaryItem[32];
+        _size = Book.Length;
+    }
+
+    /// <summary>
+    /// Метод расширающий массив в два раза, в случаи нахождений коллизии
+    /// </summary>
+    /// <param name="key">ключ</param>
+    /// <param name="value">значение</param>
+    private void ReSize(int key, string value)
+    {
+        OtusDictionaryItem[] tempBook = new OtusDictionaryItem[_size * 2];
+
+        int tempSize = tempBook.Length;
+
+        foreach (var dictionaryItem in Book)
+        {
+            if (dictionaryItem is not null)
+            {
+                int itemIndex = dictionaryItem.key % tempSize;
+
+                tempBook[itemIndex] = dictionaryItem;
+            }
+        }
+
+        Book = tempBook;
+        _size = tempSize;
+
+        // Метод добавления вынесен сюда, чтобы алгоритм увеличивал массив до тех пор, пока индекс не станет уникальным
+        Add(key, value);
+    }
+
+    /// <summary>
+    /// Метод добавления новых данных в словарь
+    /// </summary>
+    /// <param name="key">ключ</param>
+    /// <param name="value">значение</param>
+    /// <exception cref="ArgumentNullException">Значение NULL</exception>
+    internal void Add(int key, string value)
+    {
+        if (value is null || value == "")
+        {
+            throw new Exception("В словарь было переданна запись с null или пустой строкой");
+        }
+
+        OtusDictionaryItem item = new(key, value);
+
+        int index = key % _size;
+
+        // Проверка коллизии
+        if (Book[index] is null)
+        {
+            Book[index] = item;
+        }
+        // В случае совпадения ключа, перезаписывает значение
+        else if (Book[index].key == item.key)
+        {
+            Book[index].value = value;
+        }
+        // Уходит в метод расширения массива
+        else
+        {
+            ReSize(key, value);
+        }
+    }
+
+    /// <summary>
+    /// Метод нахождения объекта по ключу
+    /// </summary>
+    /// <param name="key">ключ</param>
+    /// <returns>Значение</returns>
+    internal OtusDictionaryItem Get(int key)
+    {
+        int index = key % _size;
+
+        if (Book[index] is null)
+        {
+            return null;
+        }
+        else
+        {
+            return Book[index];
+        }
+    }
+}
---
+++ 11_Dictionary/Program.cs
@@ -0,0 +1,68 @@
+﻿namespace Dictionary;
+
+internal class Program
+{
+    static void Main()
+    {
+        OtusDictionary dictionary = new();
+
+        try
+        {
+            // Добавление парочки значений в словарь
+            // Данные значения займут индексы с 0 по 19 и 31 
+            for (int i = 127; i < 148; i++)
+            {
+                dictionary.Add(i, $"строка{i}");
+            }
+
+            // Проверка на коллизии, со стандартым размером в 32, остаток от 200 будет 8
+            // Массив расширится до 128
+            // Занятые индексы будут с 0 по 19 и 127, новая строка займет индекс 72 
+            dictionary.Add(200, "новая строка200");
+
+            // Проверка метода Get
+
+            // Передадим ключ, которого нету в словаре
+            // В ответ получим null
+            Console.WriteLine(" Ключ 23213: " + dictionary.Get(23213));
+
+            // Передадим ключ, который есть в словаре
+            Console.WriteLine(" Ключ 200: " + dictionary.Get(200).value);
+
+            // Вместо методов Add и Get можно обращаться к массиву через индекс, он равен ключу
+
+            // Поскольку такой ключ уже есть, то произойдет замена значения
+            dictionary[200] = new OtusDictionaryItem(200, "еще одна новая строка");
+
+            // Выдаст значение по индексу 200
+            Console.WriteLine("Обращение по индексу 200:" + dictionary[200].value);
+
+            // Вывод
+            for (int i = 0; i < dictionary.length; i++)
+            {
+                if (dictionary[i] != null)
+                {
+                    Console.WriteLine($"Итем: {i} - {dictionary[i].key} - {dictionary[i].value}");
+                }
+                else
+                {
+                    Console.WriteLine("Итем: пустой");
+                }
+            }
+
+            // Исключения
+
+            // Если передать пустую или null строку
+            //dictionary.Add(192, null);
+            //dictionary.Add(192, "");
+
+            // Если обратиться по несуществующему индексу
+            Console.WriteLine(dictionary[241512]);
+            //dictionary[2415112] = new OtusDictionaryItem(3123, "value");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Ошибка: {e.Message}");
+        }
+    }
+}
